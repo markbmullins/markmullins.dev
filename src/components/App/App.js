179362c3964b5e198aac18b26a2de78130332fc4
markbmullins <markmullins95@gmail.com>
2020-01-28 14:44:01 -0500

    Added styled-components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,71 @@
 import React, { useState, useEffect, useCallback } from "react";
+import styled from 'styled-components';
 import { SocialIcon } from "react-social-icons";
+import { StylesProvider } from '@material-ui/core/styles';
 import Slider from "@material-ui/core/Slider";
 import Image from "../Image/Image";
-import Video from "../Video/Video";
 
-import "./App.css";
+const files = ["sunset4.png", "sun.png", "sunset5.png", "plane.png", "triangles.jpg", "sunset.jpg", "sun2.jpg", "sun3.jpg"];
+const backgrounds = files.map(file => <Image source={`${process.env.PUBLIC_URL}/backgrounds/${file}`} />)
 
-const backgrounds = [
-    <Image source={`${process.env.PUBLIC_URL}/spikes.png`} />,
-    <Image source={`${process.env.PUBLIC_URL}/sunset4.png`} />,
-    <Image source={`${process.env.PUBLIC_URL}/sun.png`} />,
-    <Image source={`${process.env.PUBLIC_URL}/sunset5.png`} />,
-    <Image source={`${process.env.PUBLIC_URL}/plane.png`} />,
-    <Image source={`${process.env.PUBLIC_URL}/triangles.jpg`} />,
-    <Image source={`${process.env.PUBLIC_URL}/sunset.jpg`} />,
-    <Image source={`${process.env.PUBLIC_URL}/sun2.jpg`} />,
-    <Image source={`${process.env.PUBLIC_URL}/sun3.jpg`} />
-];
+const StyledSlider = styled(Slider)`
+    margin-top: 10%;
+    color: #ffffff;
+`;
+
+const SocialIconContainer = styled.li`
+    margin: auto;
+`;
+
+const Name = styled.h1`
+    color: #ffffff;
+    font-family: "Monoton", cursive;
+    font-size: 100px;
+    z-index: 2;
+`;
+
+const PageContent = styled.div`
+    display: block;
+    position: absolute;
+    bottom: 35%;
+    left: 50%;
+    -webkit-transform: translateX(-50%);
+    transform: translateX(-50%);
+    z-index: 2;
+`;
+
+const IconList = styled.ul`
+    list-style: none;
+    display: flex;
+    margin-block-start: 0;
+    margin-block-end: 0;
+    margin-inline-start: 0;
+    margin-inline-end: 0;
+    padding-inline-start: 0;
+`;
+
+const BackgroundNavigation = styled.span`
+    color: white;
+    position: absolute;
+    top: 50%;
+    width: 100%;
+    height: 50px;
+`;
+
+const BackgroundChanger = styled.span`
+    font-size: 40px;
+    cursor: pointer;
+`;
+
+const NextBackground = styled(BackgroundChanger)`
+    margin-left: auto;
+    margin-right: 5%;
+`;
+
+const PreviousBackground = styled(BackgroundChanger)`
+    margin-left: 5%;
+`;
+
 function App() {
     const [backgroundNumber, setBackgroundNumber] = useState(0);
     const [changeInterval, setChangeInterval] = useState(5);
@@ -26,13 +75,13 @@ function App() {
     const changeBackground = useCallback(direction => {
         switch (direction) {
             case FORWARD:
-                backgroundNumber === backgrounds.length - 1
+                backgroundNumber === files.length - 1
                     ? setBackgroundNumber(0)
                     : setBackgroundNumber(backgroundNumber + 1);
                 break;
             case BACKWARD:
                 backgroundNumber === 0
-                    ? setBackgroundNumber(backgrounds.length - 1)
+                    ? setBackgroundNumber(files.length - 1)
                     : setBackgroundNumber(backgroundNumber - 1);
                 break;
             default:
@@ -54,56 +103,57 @@ function App() {
     }, [changeBackground, changeInterval]);
 
     return (
-        <div className="App">
-            <div className="container">
-                <h1 className="name">Mark Mullins</h1>
-
-                <ul className="social">
-                    <li className="social-icon">
-                        <SocialIcon
-                            url="https://github.com/markbmullins"
-                            target="blank"
-                            bgColor="#FFFFFF"
-                            fgColor="#000000"
-                        />
-                    </li>
-                    <li className="social-icon">
-                        <SocialIcon
-                            url="https://www.linkedin.com/in/markbmullins/"
-                            target="blank"
-                            bgColor="#FFFFFF"
-                            fgColor="#000000"
-                        />
-                    </li>
-                    <li className="social-icon">
-                        <SocialIcon
-                            url="mailto:markmullins95@gmail.com"
-                            target="blank"
-                            bgColor="#FFFFFF"
-                            fgColor="#000000"
-                        />
-                    </li>
-                </ul>
-                <Slider
-                    value={changeInterval}
-                    onChange={handleSlider}
-                    aria-labelledby="continuous-slider"
-                    valueLabelDisplay="auto"
-                    min={0}
-                    max={10}
-                    id="slider"
-                />
+        <StylesProvider injectFirst>
+            <div>
+                <PageContent>
+                    <Name>Mark Mullins</Name>
+                    <IconList>
+                        <SocialIconContainer>
+                            <SocialIcon
+                                url="https://github.com/markbmullins"
+                                target="blank"
+                                bgColor="#FFFFFF"
+                                fgColor="#000000"
+                            />
+                        </SocialIconContainer>
+                        <SocialIconContainer>
+                            <SocialIcon
+                                url="https://www.linkedin.com/in/markbmullins/"
+                                target="blank"
+                                bgColor="#FFFFFF"
+                                fgColor="#000000"
+                            />
+                        </SocialIconContainer>
+                        <SocialIconContainer>
+                            <SocialIcon
+                                url="mailto:markmullins95@gmail.com"
+                                target="blank"
+                                bgColor="#FFFFFF"
+                                fgColor="#000000"
+                            />
+                        </SocialIconContainer>
+                    </IconList>
+                    <StyledSlider
+                        value={changeInterval}
+                        onChange={handleSlider}
+                        aria-labelledby="continuous-slider"
+                        valueLabelDisplay="auto"
+                        min={0}
+                        max={10}
+                        id="slider"
+                    />
+                </PageContent>
+                {backgrounds[backgroundNumber]}
+                <BackgroundNavigation>
+                    <PreviousBackground onClick={() => changeBackground(BACKWARD)}>
+                        {"<"}
+                    </PreviousBackground>
+                    <NextBackground onClick={() => changeBackground(FORWARD)}>
+                        {">"}
+                    </NextBackground>
+                </BackgroundNavigation>
             </div>
-            {backgrounds[backgroundNumber]}
-            <div className="image-navigation">
-                <span className="changer-button left" onClick={() => changeBackground(BACKWARD)}>
-                    {"<"}
-                </span>
-                <span className="changer-button right" onClick={() => changeBackground(FORWARD)}>
-                    {">"}
-                </span>
-            </div>
-        </div>
+        </StylesProvider>
     );
 }
 
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,7 +1,30 @@
-import React from "react";
-import "./Image.scss";
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledImage = styled.img`
+    /* Set rules to fill background */
+    min-height: 100%;
+    min-width: 1024px;
+      
+    /* Set up proportionate scaling */
+    width: 100%;
+    height: auto;
+      
+    /* Set up positioning */
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+  
+  @media screen and (max-width: 1024px) {
+    img.image {
+      left: 50%;
+      margin-left: -512px;   /* 50% */
+    }
+`;
+
 const Image = ({ source }) => {
-    return <img className="image" src={source} alt="sun"></img>;
+    return <StyledImage src={source} alt="A background image"></StyledImage>;
 };
 
 export default Image;
